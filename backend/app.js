da54e7dba79b0c4fb6caf9df49374d6597c27df1
Pushpendra Pal <p_pal@bt.iitr.ac.in>
2024-03-30 01:39:13 +0530

    backend and fit-band-server setup

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const user = require("./routes/userRoute");
+
+app.use("/api/v1", user);
+
+module.exports = app;
